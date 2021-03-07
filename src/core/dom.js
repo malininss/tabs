31676cbc5afaf913a98060e51c28cc03be316f4f
Sergey Malinin <malinin@web.1tv.ru>
2021-03-07 10:33:36 +0300

    finish table resize

--- src/core/dom.js
+++ src/core/dom.js
@@ -40,6 +40,27 @@ class Dom {
 
     return this;
   }
+
+  get data() {
+    return this.$el.dataset;
+  }
+
+  closest(selector) {
+    return $(this.$el.closest(selector));
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect();
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector);
+  }
+
+  css(styles = {}) {
+    Object.assign(this.$el.style, styles);
+    return this;
+  }
 }
 
 export function $(selector) {
